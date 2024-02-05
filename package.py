25d2336c2534fbf75890db55365fbfbd330c9c36
BlackTechX <155609122+BlackTechX011@users.noreply.github.com>
2024-02-05 21:45:37 +0530

    Update

---
+++ package.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import os.path
+from os import path
+
+import json
+
+projects = {}
+
+with open("projects.json") as f:
+    projects = json.load(f)
+
+index_json = {
+    "packages": []
+}
+
+for i, project in enumerate(projects):
+    # Meta data
+    project_json = {
+        "name": project,
+        "maintainer": "BlackTechX011",
+        "websiteURL": "https://github.com/BlackTechX011",
+        "help": {
+            "online": f"https://github.com/BlackTechX011",
+        },
+        "platforms": [],
+        "tools": [],
+    }
+    
+    # Platforms
+    for package in projects[project]:
+        platform_file = f"{project}/{package}-platform.json"
+        
+        if path.exists(platform_file):
+            with open(platform_file) as f:
+                platforms = json.load(f)
+                for p in platforms:
+                    project_json["platforms"].append(p)
+
+        # Tools
+        tools_file = f"{project}/{package}/tools.json"
+        with open(tools_file) as f:
+            tools = json.load(f)
+            for t in tools:
+                project_json["tools"].append(t)
+
+    index_json["packages"].append(project_json)
+
+with open("package_BlackTechX_index.json", "w") as f:
+    f.write(json.dumps(index_json, indent=4))
